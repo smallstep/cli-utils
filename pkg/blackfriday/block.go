45e754faab6e843ff5169303a71c79eba9d6d519
Herman Slatman <hermanslatman@hotmail.com>
2025-05-26 18:11:19 +0200

    Add `staticcheck` to ignored linters for `blackfriday` package

--- pkg/blackfriday/block.go
+++ pkg/blackfriday/block.go
@@ -11,7 +11,7 @@
 // Functions to parse block-level elements.
 //
 
-//nolint:gocritic,revive,ineffassign,gosimple,wastedassign // ignore blackfriday
+//nolint:gocritic,revive,ineffassign,gosimple,wastedassign,staticcheck // ignore blackfriday
 package blackfriday
 
 import (
